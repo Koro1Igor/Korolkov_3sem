09b93ce3773a69b3a0aba92f2d3465c6b51c9cf1
Koro1Igor <korolkovigor021@gmail.com>
2025-11-09 19:28:29 +0300

    Refactor library classes to shop classes

--- rk1.py
+++ rk1.py
@@ -2,34 +2,34 @@ from operator import itemgetter
 
 class Book:
     """Книга"""
-    def __init__(self, id, title, price, lib_id):
+    def __init__(self, id, title, price, shop_id):
         self.id = id
         self.title = title
         self.price = price
-        self.lib_id = lib_id
+        self.shop_id = shop_id
 
 
-class Library:
-    """Библиотека"""
+class Shop:
+    """Книжный магазин"""
     def __init__(self, id, name):
         self.id = id
         self.name = name
 
 
-class BookLibrary:
-    """Связь книги и библиотеки (многие-ко-многим)"""
-    def __init__(self, lib_id, book_id):
-        self.lib_id = lib_id
+class BookShop:
+    """Связь книги и магазина (многие-ко-многим)"""
+    def __init__(self, shop_id, book_id):
+        self.shop_id = shop_id
         self.book_id = book_id
 
 
 # --- Данные ---
-libs = [
-    Library(1, 'Академическая библиотека'),
-    Library(2, 'Архивная библиотека №1'),
-    Library(3, 'Городская библиотека'),
-    Library(4, 'Областная библиотека'),
-    Library(5, 'Арт-библиотека')
+shops = [
+    Shop(1, 'Альфа-Книги'),
+    Shop(2, 'Арбатская книга'),
+    Shop(3, 'Город книг'),
+    Shop(4, 'Мир литературы'),
+    Shop(5, 'Астра-Книга'),
 ]
 
 books = [
@@ -42,18 +42,18 @@ books = [
     Book(7, 'А зори здесь тихие', 480, 5),
 ]
 
-books_libs = [
-    BookLibrary(1, 1),
-    BookLibrary(1, 2),
-    BookLibrary(2, 5),
-    BookLibrary(3, 3),
-    BookLibrary(3, 4),
-    BookLibrary(4, 6),
-    BookLibrary(5, 7),
+books_shops = [
+    BookShop(1, 1),
+    BookShop(1, 2),
+    BookShop(2, 5),
+    BookShop(3, 3),
+    BookShop(3, 4),
+    BookShop(4, 6),
+    BookShop(5, 7),
     # связи многие-ко-многим
-    BookLibrary(1, 3),
-    BookLibrary(2, 4),
-    BookLibrary(5, 1)
+    BookShop(1, 3),
+    BookShop(2, 4),
+    BookShop(5, 1)
 ]
 
 
@@ -61,41 +61,41 @@ def main():
     """Основная функция"""
 
     # --- Один-ко-многим ---
-    one_to_many = [(b.title, b.price, l.name)
-                   for l in libs
+    one_to_many = [(b.title, b.price, s.name)
+                   for s in shops
                    for b in books
-                   if b.lib_id == l.id]
+                   if b.shop_id == s.id]
 
     # --- Многие-ко-многим ---
-    many_to_many_temp = [(l.name, bl.lib_id, bl.book_id)
-                         for l in libs
-                         for bl in books_libs
-                         if l.id == bl.lib_id]
+    many_to_many_temp = [(s.name, bs.shop_id, bs.book_id)
+                         for s in shops
+                         for bs in books_shops
+                         if s.id == bs.shop_id]
 
-    many_to_many = [(b.title, b.price, lib_name)
-                    for lib_name, lib_id, book_id in many_to_many_temp
+    many_to_many = [(b.title, b.price, shop_name)
+                    for shop_name, shop_id, book_id in many_to_many_temp
                     for b in books if b.id == book_id]
 
     # === Задание Г1 ===
     print('Задание Г1')
     res_1 = {}
-    for l in libs:
-        if l.name.startswith('А'):
-            # Список книг библиотеки
-            l_books = list(filter(lambda i: i[2] == l.name, one_to_many))
-            book_titles = [x for x, _, _ in l_books]
-            res_1[l.name] = book_titles
+    for s in shops:
+        if s.name.startswith('А'):
+            # Список книг магазина
+            s_books = list(filter(lambda i: i[2] == s.name, one_to_many))
+            book_titles = [x for x, _, _ in s_books]
+            res_1[s.name] = book_titles
     print(res_1)
 
     # === Задание Г2 ===
     print('\nЗадание Г2')
     res_2_unsorted = []
-    for l in libs:
-        l_books = list(filter(lambda i: i[2] == l.name, one_to_many))
-        if len(l_books) > 0:
-            prices = [price for _, price, _ in l_books]
+    for s in shops:
+        s_books = list(filter(lambda i: i[2] == s.name, one_to_many))
+        if len(s_books) > 0:
+            prices = [price for _, price, _ in s_books]
             max_price = max(prices)
-            res_2_unsorted.append((l.name, max_price))
+            res_2_unsorted.append((s.name, max_price))
 
     res_2 = sorted(res_2_unsorted, key=itemgetter(1), reverse=True)
     print(res_2)
